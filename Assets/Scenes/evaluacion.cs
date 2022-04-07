4eb2375698ee0b30c527d7e2ade9ef8c63b6b5f5
pedrogaldo <galdo.pedro13@gmail.com>
2022-04-07 15:06:52 -0300

    terminado

--- Assets/Scenes/evaluacion.cs
+++ Assets/Scenes/evaluacion.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class evaluacion : MonoBehaviour
 {
+    public int n1;
+    public int n2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (n1 == n2)
+        {
+            Debug.Log("los numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("los numeros son distintos");
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scenes/evaluacion2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class evaluacion2 : MonoBehaviour
+{
+    public int monto;
+    public int precio1;
+    public int precio2;
+    public int precio3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int preciototal = precio1 + precio2 + precio3;
+        int faltan = preciototal - monto;
+        int sobran = monto - preciototal;
+
+        if (preciototal > monto)
+
+        {
+            Debug.Log("no alcanza el monto disponible, faltan $" + faltan);
+        }
+
+        else if (preciototal<=monto)
+        {
+            Debug.Log("alcanza el monto disponible y sobran $" + sobran);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
